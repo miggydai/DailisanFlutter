267d6d7741c5622c323227829166a1b91dd19772
Migs <miguelemmanueldailisan@gmail.com>
2023-02-18 18:30:53 +0800

    updated card

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -10,41 +10,60 @@ class MyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * .5 ,
-              height: 300,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/iphone-transparent.png"),
-                    fit: BoxFit.fill,
-                  ),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10))),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-             Padding(
-               padding: const EdgeInsets.only(right:150.0, bottom: 8),
-               child: Text(
-                  "iphone", textAlign: TextAlign.center,
-                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15)
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * .5,
+            height: 300,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/iphone-transparent.png"),
+                  fit: BoxFit.fill,
                 ),
-             ),
-
-            Padding(
-              padding: const EdgeInsets.only(right:100.0),
-              child: ElevatedButton.icon(onPressed: () {}, icon: Icon(FontAwesomeIcons.cartShopping, size: 15,), label: Text("add to cart", style: TextStyle(fontWeight: FontWeight.w500 ),), style: ElevatedButton.styleFrom(primary: Color.fromRGBO(107, 137, 232, 1)),),
-            )
-          ],
-        ),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10))),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 150.0, bottom: 2),
+            child: Text("iPhone 12X",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 190.0, bottom: 8),
+            child: Text("Apple",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15,
+                    color: Colors.grey)),
+          ),
+          // Padding(
+          //   padding: const EdgeInsets.only(right: 100.0),
+          //   child: ElevatedButton.icon(
+          //     onPressed: () {},
+          //     icon: Icon(
+          //       FontAwesomeIcons.cartShopping,
+          //       size: 15,
+          //     ),
+          //     label: Text(
+          //       "add to cart",
+          //       style: TextStyle(fontWeight: FontWeight.w500),
+          //     ),
+          //     style: ElevatedButton.styleFrom(
+          //         primary: Color.fromRGBO(107, 137, 232, 1)),
+          //   ),
+          // )
+        ],
+      ),
     );
   }
 }
